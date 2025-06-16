a288290541c19fa01cf8026214b73382ad93cacf
Neiromen <alex.kor0702@gmail.com>
2025-06-16 23:43:40 +0300

    Annotation @RequestParam

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -20,9 +21,16 @@ public class MyController {
 //    public String showEmployeeDetails(){
 //        return "show-employee-details-view";
 //    }
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. "+empName;
+//        model.addAttribute("nameAttribute",empName);
+//        return "show-employee-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("employeeName");
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model){
         empName = "Mr. "+empName;
         model.addAttribute("nameAttribute",empName);
         return "show-employee-details-view";
--- src/main/webapp/WEB-INF/view/show-employee-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-employee-details-view.jsp
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-<h2>Debug Information:</h2>
-<p>Request parameters: ${employeeName}</p>
-<p>nameAttribute: ${nameAttribute}</p>
-<p>Class: ${nameAttribute.getClass()}</p>
+<h2>Dear Employee, you are WELCOME!</h2>
+<p>${nameAttribute}</p>
 </body>
 </html>
